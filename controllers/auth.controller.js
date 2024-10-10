c280d3835f05476ee6e7d09e88d6f5c2bc4d1d79
Pramit Bhatia <pramitbhatia25@gmail.com>
2024-10-10 17:02:42 -0400

    Edits

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,3 @@
-import {User} from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import {generateVerificationToken} from "../utils/generateVerificationToken.js";
 import {generateTokenAndSetCookie} from "../utils/generateTokenAndSetCookie.js";
